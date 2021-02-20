543ab67f44998af4c5f17220977cfd990fecf05a
cetRide <okolacetric@gmail.com>
2021-02-21 00:31:09 +0300

    error handling and migrations

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.9.0
 )
---
+++ infrastructure/repository/migration/000001_rideyu_schema.up.sql
@@ -0,0 +1,44 @@
+-- migrate -path migration -database "postgresql://rideyu:password@localhost:5432/pexs?sslmode=disable" -verbose up
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TABLE "users"(
+   "id" bigserial PRIMARY KEY,
+   "username" varchar (50) UNIQUE NOT NULL,
+   "phone" varchar (300) UNIQUE NOT NULL,
+   "email" varchar (300) UNIQUE NOT NULL,
+   "firstname" varchar,
+   "lastname" varchar,
+   "password" varchar (50) NOT NULL,
+   "cover_photo" varchar,
+   "profile_picture" varchar,
+   "created_at" timestamp NOT NULL DEFAULT (now()),
+   "updated_at"  timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "users" ("username");
+CREATE INDEX ON "users" ("email");
+CREATE INDEX ON "users" ("phone");
+
+-- ON UPDATE - set current_timestamp (trigger).
+DO $$
+DECLARE
+    t record;
+BEGIN
+    FOR t IN 
+        SELECT * FROM information_schema.columns
+        WHERE column_name = 'updated_at'
+    LOOP
+        EXECUTE format('CREATE TRIGGER set_updated_at
+                        BEFORE INSERT ON %I.%I
+                        FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp()',
+                        t.table_schema, t.table_name);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
